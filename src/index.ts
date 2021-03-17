803d716e32f464edf80c2b2659a59f8d7c301de2
risaddex <risaddex@gmail.com>
2021-03-17 11:43:56 -0300

    Added project 1 and nodemon config

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import express from 'express'
+
+const app = express()
+
+// api index
+app.get('/', (req, res) => {
+  res.send('Hello word')
+})
+
+// if no parameters, shows current Date
+app.get('/api/timestamp', (req, res) => {
+  let date = new Date()
+  res.send({
+    utc: date.toUTCString(),
+    unix: date.getTime(),
+  })
+})
+
+app.get('/api/timestamp/:date', (req, res) => {
+  // requirement: acept unix ms number as param
+  const dateParams = isNaN(Number(req.params.date))
+    ? req.params.date
+    : Number(req.params.date)
+
+  const timeResponse = new Date(dateParams)
+
+  // Handles invalid date
+  if (isNaN(timeResponse.getTime())) {
+    res.send({
+      error: 'Invalid Date',
+    })
+  } else {
+    const utcResponse = timeResponse.toUTCString()
+    const unixDate = timeResponse.getTime()
+
+    res.send({
+      utc: utcResponse,
+      unix: unixDate,
+    })
+  }
+})
+
+// SERVER INIT
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`)
+})
