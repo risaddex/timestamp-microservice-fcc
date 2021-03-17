211eaabbcb6df1ab9ef11d1c39d0baa669e5d7e9
risaddex <risaddex@gmail.com>
2021-03-17 12:26:44 -0300

    added main file

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,7 @@ app.get('/api/timestamp/:date', (req, res) => {
 
 // SERVER INIT
 const port = process.env.PORT || 3000
+
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
 })
